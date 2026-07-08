c108b1c492adf7c0770e82b33ec24cd5628a0daa
itshowrohitworks <therohitshrivastava@gmail.com>
2026-07-08 16:45:43 +0530

    Added custom CNN model and forward pass.

---
+++ src/models/cnn.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+
+class CatDogCNN(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Convolution Layer:
+        self.features = nn.Sequential(
+            
+            # input shape: 3 x 224 x 224
+
+            nn.Conv2d(in_channels=3,out_channels=32,kernel_size=3,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(in_channels=32,out_channels=64,kernel_size=3,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(in_channels=64,out_channels=128,kernel_size=3,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        """
+        till here the output shape: 
+        128 x 28 x 28
+        """
+
+
+        # Linear Layer: Flatten Layer
+        self.classifier = nn.Sequential(
+            
+            # input shape: 128 x 28 x 28
+
+            nn.Flatten(),
+
+            nn.Linear(in_features=128 * 28 * 28,out_features=512),
+            nn.ReLU(),
+
+            # Dropout: regularization technique -> to prevent overfitting during training
+            nn.Dropout(p=0.5),
+
+            nn.Linear(in_features=512,out_features=2)
+        )
+        """
+        till here the output:
+        2 Classes: 
+        Cat and Dog
+        """
+    
+    def forward(self,x):
+        
+        x = self.features(x)
+
+        x = self.classifier(x)
+
+        return x
